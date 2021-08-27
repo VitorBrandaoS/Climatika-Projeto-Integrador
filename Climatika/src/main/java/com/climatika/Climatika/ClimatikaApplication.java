6ac9508f16f251626872d007eab5bec8e7e5f8f3
unknown <sdearaujo.rodrigo@gmail.com>
2021-08-27 13:58:59 -0300

    [Feature Rodrigo - Task: Criado classes Produto e StatusVenda]

---
+++ Climatika/src/main/java/com/climatika/Climatika/ClimatikaApplication.java
@@ -0,0 +1,13 @@
+package com.climatika.Climatika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimatikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimatikaApplication.class, args);
+	}
+
+}
