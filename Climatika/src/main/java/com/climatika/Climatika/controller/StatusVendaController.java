e89e058536e49c8070802cd1e853645a2ac33b61
Agatha Nara <gthlbrc@outlook.com>
2021-09-01 16:15:45 -0300

    [Feature Agatha - Task: Corrigido notação dos controllers]

--- Climatika/src/main/java/com/climatika/Climatika/controller/StatusVendaController.java
+++ Climatika/src/main/java/com/climatika/Climatika/controller/StatusVendaController.java
@@ -15,11 +15,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.climatika.Climatika.models.StatusVenda;
 import com.climatika.Climatika.repository.StatusVendaRepository;
 
-@Entity
+@RestController
 @RequestMapping("/climatika/Status")
 @CrossOrigin("*")
 public class StatusVendaController {
