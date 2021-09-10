04a16901b88da4702a87b4801a460c6e38714f40
isaiaszanoni <isaiasz.mus@gmail.com>
2021-09-10 11:01:24 -0300

    [Feature Isaias - Task: Add class UserDetailsImpl]

---
+++ Climatika/src/main/java/com/climatika/Climatika/Securanca/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package com.climatika.Climatika.Securanca;
+
+import java.util.List;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails{
+
+	private static final Long serialVersionlUID = 1L;
+
+	private String email;
+	private String password;
+	private List<GrantedAuthority> authorization;
+
+	public UserDetailsImpl(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorization;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
